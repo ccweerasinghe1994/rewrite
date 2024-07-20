d227b1820aa5616f6f95d562526d758e6f3826d0
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-07-20 20:02:28 +0530

    initial commit

---
+++ src/test/java/dev/danvega/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+package dev.danvega;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Test
+    void shouldAddTwoStringNumbers() {
+        StringCalculator stringCalculator = new StringCalculator();
+        int sum = stringCalculator.add("1,2");
+        assertEquals(3, sum);
+    }
+
+    @Test
+    void shouldSubtractTwoNumbers() {
+        StringCalculator stringCalculator = new StringCalculator();
+        int difference = stringCalculator.subtract("3,2");
+        assertEquals(1, difference);
+    }
+
+}
