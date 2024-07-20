03cc96dccc653e057bd8e12ab3077e99f2f4a2bd
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-07-20 20:34:25 +0530

    Update free-style.sh

--- free-style.sh
+++ free-style.sh
@@ -1,2 +1,2 @@
 /var/jenkins_home/.local/state/fnm_multishells/228_1721483982768/bin/node -v
-/var/jenkins_home/.local/state/fnm_multishells/228_1721483982768/bin/node -v
+/var/jenkins_home/.local/state/fnm_multishells/228_1721483982768/bin/npm -v
